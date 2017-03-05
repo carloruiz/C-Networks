1ad2f9c079939d10934c93f4543d99a876545726
csr2131 <csr2131@columbia.edu>
2017-03-05 05:49:21 +0000

    Client Table. Comm down

--- udpChat.c
+++ udpChat.c
@@ -9,36 +9,81 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
-
-//#include <pthreads.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 
 #define BUFSIZE 1024
 
+int maxSz = 10;
+int currSz = 0;
+
+struct entry {
+	char uname[25];
+	struct sockaddr_in clntaddr;
+	char status;
+};
+
+struct servthread_args {
+    char *buf;
+    struct sockaddr_in *clntaddr;
+};
+
+struct entry *TABLE;
+
+int getACK(char *, int, struct sockaddr_in *, socklen_t *);
 void udpServer(char *);
 void udpClient(char *, char *, char *, char *);
-int getACK(char *, int, struct sockaddr_in *, socklen_t *);
+void initTABLE(void);
+void resizeTABLE(void);
+void addEntry(char *, struct sockaddr_in *);
+void broadcastTABLE(void);
 
-void error(char *msg) {
-    perror(msg);
+
+void error(char *buf) {
+    perror(buf);
     exit(1);
 }
 
-void die(char *msg) {
-	fprintf(stderr, "%s\n", msg);
+void die(char *buf) {
+	fprintf(stderr, "%s\n", buf);
 	exit(1);
 }
 
-struct servthread_args {
-    char *msg;
-    struct sockaddr_in clntaddr;
-};
-
-void serverRequest(void *ptr)
+void *serverRequest(void *ptr)
 {
-	struct servthread_args *args = (struct servthread_args *)ptr;
+	int len;
+	char *buf;
+	struct sockaddr_in *clntaddr;
+	struct servthread_args *args;
+
+	args = (struct servthread_args *)ptr;	
+	buf = args->buf;
+	clntaddr = args->clntaddr;
+
+	fprintf(stderr, "Now pthread has %s\n", buf);
 	
+	len = strlen(buf);
+
+	// Can recieve three types of messages 
+	// [INIT]
+	// reg/de-reg
+	// [STORE] messages
+
+	if (strstr(buf, "[INIT]") == buf) {
+		char *uname = strstr(buf, "]") + 1;
+		addEntry(uname, args->clntaddr);
+		broadcastTABLE();
+	}	
+
+	free(args->buf);
+	free(args->clntaddr);
+	free(args);
+
+	return NULL;	
 }
+
+
 
 int main(int argc, char **argv) {
 
@@ -57,16 +102,16 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+
+
 void udpServer(char *port_str)
 {
-	fprintf(stderr, "In server\n\n");
-    int servSock, port;
-    struct sockaddr_in servaddr, clntaddr; //tmp
-    struct hostent *hostp;
-    char buf[BUFSIZE];
-    char *hostaddrp;
-    int n;
+    int servSock, port, n;
+    struct sockaddr_in servaddr;
+    struct servthread_args *args;
     socklen_t clntlen;
+	pthread_t tid;
+	char ipstr[20];
 
     port = atoi(port_str);
 
@@ -84,126 +129,146 @@ void udpServer(char *port_str)
     if (bind(servSock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
         error("ERROR binding");
 
+	initTABLE();
 	fprintf(stderr, "server up..LESSGOOO..GET THIS CHAT GOIIN\n");
 
     clntlen = sizeof(struct sockaddr_in);
 
     for ( ; ;) {
-
-    /*  tmp = malloc(sizeof(struct sockaddr_in));
-        memcpy(tmp, clntaddr, sizeof(clntaddr));
-        temp->msg = malloc(BUFSIZE);
-        recvfrom(servSock, temp->msg, BUFSIZE etc...
-    */
-        //buf = malloc(BUFSIZE);
 		
-		fprintf(stderr, "wating on client\n");
-
-        n = recvfrom(servSock, buf, BUFSIZE, 0,
-                        (struct sockaddr *)&clntaddr, &clntlen);
-		
+		args = malloc(sizeof(*args));
+        args->clntaddr = malloc(sizeof(struct sockaddr_in));
+        args->buf = malloc(BUFSIZE);
+        n = recvfrom(servSock, args->buf, BUFSIZE, 0,
+						(struct sockaddr *)args->clntaddr, &clntlen);
+	
         if (n < 0) {
             perror("ERROR on recvfrom()");
             continue;
         }
 		
-		fprintf(stderr, "recieved %s from client\n", buf); 
+		fprintf(stderr, "recieved %s from client with address %s :%u\n", args->buf, inet_ntop(AF_INET, &(args->clntaddr->sin_addr.s_addr), ipstr, 20), ntohs(args->clntaddr->sin_port)); 
 			
-        if (sendto(servSock, "ACK", 4, 0, (struct sockaddr *)&clntaddr, clntlen) < 4) {
+        if (sendto(servSock, "ACK", 4, 0, (struct sockaddr *)args->clntaddr, clntlen) < 4){
 			perror("sending ACK failed\n");
             continue;
         }
 
 		fprintf(stderr, "ACK sent to client\n");
-        //buf[BUFSIZE - 1] = '\0';
-        //pthread_create(&tid, NULL, serverRequest, temp);
-    }
+        
+		//make sure buff is null terminated
+		args->buf[BUFSIZE - 1] = '\0';
+        pthread_create(&tid, NULL, serverRequest,args);
+    } //end for(; ;)
 }
+
+
+
+
+
+
 
 void udpClient(char *uname, char *servIP, char *servPort, char *clntPort)
 {
-	fprintf(stderr, "In client\n\n");
-	fprintf(stderr, "%s\n%s\n%s\n%s\n", uname, servIP, servPort, clntPort);
-
-	
-	int servSock, mySock, port, n,i;
+	int servSock, mySock, n, i;
 	struct sockaddr_in servAddr, myAddr;
 	char buf[BUFSIZE];
 	socklen_t servlen, mylen;
-
+	pthread_t tid;
+	int val = 1;
 	servlen = mylen =  sizeof(struct sockaddr_in);
-	
-	// Creating my socket and binding it to given port
-	if ((mySock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-		error("socket() failed");
 
-	memset(&myAddr, 0, sizeof(myAddr));
-	myAddr.sin_family = AF_INET;
-	myAddr.sin_port = htons(atoi(clntPort));
-	if (inet_aton(servIP, &myAddr.sin_addr) == 0)
-		error("inet_aton() failed");
-
-	if (bind(mySock, (struct sockaddr *)&myAddr, sizeof(myAddr)) < 0)
-		error("ERROR on bind()");
-
-	// Creating server socket with given args
-	if ((servSock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-		error("socket() failed");
-
+	// server Address
 	memset(&servAddr, 0, sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htons(atoi(servPort));
 	if (inet_aton(servIP, &servAddr.sin_addr) == 0)
 		error("inet_aton() failed");
 
-	//set servSock time out to 500 millies
+	// my Address
+	memset(&myAddr, 0, sizeof(myAddr));
+	myAddr.sin_family = AF_INET;
+	myAddr.sin_port = htons(atoi(clntPort));
+	if (inet_aton(servIP, &myAddr.sin_addr) == 0)
+		error("inet_aton() failed");
+
+
+	// nn blocking Socket will test server/peer's status
+	if ((mySock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		error("socket() failed");
+	
+	setsockopt(mySock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int));
+	if (bind(mySock, (struct sockaddr *)&myAddr, sizeof(myAddr)) < 0)
+		error("ERROR on bind()");
+
+	// Blocking socket that will wait for changes in Client Table
+	if ((servSock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		error("socket() failed");
+
+	setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int));
+	if (bind(servSock, (struct sockaddr *)&myAddr, sizeof(myAddr)) < 0)
+		error("ERROR on bind()");
+
+	// creating socket that times out..To be used to test clients
+	// and server status
+
 	struct timeval tv;
 	tv.tv_sec = 0;
 	tv.tv_usec = 500 * 1000;
-	setsockopt(servSock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+	setsockopt(mySock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+	
+	//set peersock vals identical
 
-	snprintf(buf, BUFSIZE, "%s|%s", uname, clntPort);
+
+	initTABLE();
+	struct entry *T = TABLE;
+	//add user input processing
+
+	snprintf(buf, BUFSIZE, "[INIT]%s", uname);
 	
 	fprintf(stderr, "sending %s to server\n", buf);
 
 
-	n = sendto(servSock, buf, strlen(buf) + 1, 0, (struct sockaddr *)&servAddr, servlen);
+	n = sendto(mySock, buf, strlen(buf) + 1, 0, 
+						(struct sockaddr *)&servAddr, servlen);
+	
 	if (n < 0)
 		error("ERROR sending packet to server");
 
+	if (getACK(buf, mySock, &servAddr, &mylen) == 0)
+		die(">>> [Server not responding]\n>>> [Exiting]");
 
-/*
-	i = 0;
-	while (i++ < 5) {
-		fprintf(stderr, "Waiting for ACK: %d\n", i);
-		n = recvfrom(servSock, buf, BUFSIZE, 0,
-							(struct sockaddr *)&servAddr, &mylen);
-		if (n > 0)
-			break;
+	//pthread_create(&tid, NULL, usrRequest,args);
+	
+	for(; ;) {
+		n = recvfrom(mySock, buf, BUFSIZE, 0,
+						(struct sockaddr *)&servAddr, &servlen);
+		/*if (n < 0) {
+			fputs("ERROR recieving packet\n");
+			continue;
+		}*/	
+		if (strstr(buf, "[UPDATE]") == buf) {
+			currSz =atoi(strstr(buf, "]") + 1);
+			if (currSz > maxSz - 1)
+				resizeTABLE();
+			for (i = 0; i < currSz; i++) 
+				n = recvfrom(servSock, &T[i], sizeof(struct entry), 0,
+						(struct sockaddr *)&servAddr, &servlen);
+			for (i = 0; i < currSz; i++)
+				fprintf(stderr, "%d: %s, %u\n", i, T[i].uname, ntohs(T[i].clntaddr.sin_port));
+			
+			fprintf(stderr,">>>[Client table updated]\n>>>");
+		}
 	}
-
-	if (i == 6)
-		die(">>> [Server not responding]\n>>> [Exiting]");
-
-	if (strcmp(buf, "ACK") != 0)
-		error("No acknowldgement recieved from server");
-*/
-
-	if (getACK(buf, servSock, &servAddr, &mylen) == 0)
-		die(">>> [Server not responding]\n>>> [Exiting]");
-
-	fprintf(stderr, "connection successful\n");
-
-	exit(1);
 	
 }
 
 int getACK(char *buf, int servSock, struct sockaddr_in *servAddr, socklen_t *len)
 {
-	int i = 0;	
+	int i, n; 
 	
+	i = 0;	
 	while (i++ < 5) {
-		fprintf(stderr, "Waiting for ACK: %d\n", i);
 		n = recvfrom(servSock, buf, BUFSIZE, 0,
 							(struct sockaddr *)servAddr, len);
 		if (n > 0 && strcmp(buf, "ACK") == 0)
@@ -212,7 +277,60 @@ int getACK(char *buf, int servSock, struct sockaddr_in *servAddr, socklen_t *len
 	return 0;
 }
 
+void broadcastTABLE(void)
+{
+	int i,clntSock, j;
+	char msg[30];
+	struct entry *T;
+	socklen_t clntlen;	
+	
+	T = TABLE;
+	i = 0;	
+	clntlen = sizeof(struct sockaddr_in);
+	sprintf(msg, "[UPDATE]%d", currSz);
+	
+	if ((clntSock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		error("socket() failed");
 
+	for (i = 0; i < currSz; i++) {
+		if (T[i].status == 1) {
+			sendto(clntSock, msg, strlen(msg)+1, 0,(struct sockaddr *)&(T[i].clntaddr)						, clntlen);
+		
+			for (j = 0; j < currSz; j++) {
+				sendto(clntSock, &T[j], sizeof(struct entry), 0,
+								(struct sockaddr *)&(T[i].clntaddr), clntlen);
+			}
+		}
+	}		
+}
+
+
+void initTABLE(void)
+{
+	TABLE = malloc(maxSz*sizeof(struct entry));
+}
+
+void resizeTABLE(void)
+{
+	maxSz *= 2;
+	struct entry *tmp = malloc(maxSz*sizeof(struct entry));
+	memcpy(tmp, TABLE, (maxSz/2)*sizeof(struct entry));
+	free(TABLE);
+	TABLE = tmp;
+}
+
+void addEntry(char *uname, struct sockaddr_in *clntaddr)
+{	
+	struct entry *T = TABLE;
+
+	strcpy(T[currSz].uname, uname);
+	memcpy(&(T[currSz].clntaddr), clntaddr, sizeof(struct sockaddr_in));
+	T[currSz].status = 1;
+	
+	if (currSz == maxSz - 1)
+		resizeTABLE();	
+	currSz++;
+}
 
 
 
